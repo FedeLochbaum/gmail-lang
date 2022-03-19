2b7a627fdad47774707e17715c874a44f8782339
Federico Lochbaum <federico.lochbaum@gmail.com>
2022-03-19 01:16:44 -0300

    doing work jest

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import {LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, delimitedIndent} from "@codemirror/language"
 import { parserWithMetadata } from './parser'
 
+export const parser = parserWithMetadata
+
 export const GmailLanguage = LRLanguage.define({
   parser: parserWithMetadata,
   languageData: {
