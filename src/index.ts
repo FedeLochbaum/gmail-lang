c8da191c3899c516e4e9abaf6f64e7d15c67e41e
Federico Lochbaum <federico.lochbaum@gmail.com>
2022-03-19 00:36:14 -0300

    following template

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import {LRLanguage, LanguageSupport, indentNodeProp, foldNodeProp, foldInside, delimitedIndent} from "@codemirror/language"
+import { parserWithMetadata } from './parser'
+
+export const GmailLanguage = LRLanguage.define({
+  parser: parserWithMetadata,
+  languageData: {
+    commentTokens: {line: ";"}
+  }
+})
+
+export default () => new LanguageSupport(GmailLanguage)
