abf91c1cb33d3e5521ad962ce05598378548d47c
Federico Lochbaum <federico.lochbaum@gmail.com>
2022-06-12 18:01:15 -0300

    addinng keywords

--- src/style.js
+++ src/style.js
@@ -1,7 +1,7 @@
 
 import { styleTags, tags as t } from '@codemirror/highlight'
 
-const keywords = 'Keyword Keyword'
+const keywords = 'Keyword Keyword from to cc bcc subject label has list filename after before older newer older_than newer_than is in deliveredto category size larger smaller'
 const strings = 'String'
 const IDS = 'ID ShortID Email'
 const logicOp = 'Not not And and Or or Minus minus'
