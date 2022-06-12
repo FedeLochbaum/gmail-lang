06ddd4b3fd0db103acaf17ae5e5b10cbe93d6ad6
Federico Lochbaum <federico.lochbaum@gmail.com>
2022-06-12 17:52:53 -0300

    updating style defs

--- src/style.js
+++ src/style.js
@@ -1,15 +1,16 @@
 
 import { styleTags, tags as t } from '@codemirror/highlight'
 
-const keywords = 'Of of Then then Otherwise otherwise It it When when Wildcard wildcard'
+const keywords = 'Keyword Keyword'
 const strings = 'String'
+const IDS = 'ID ShortID Email'
 const logicOp = 'Not not And and Or or Minus minus'
 const paren = '( )'
 
 export default styleTags({
   Boolean: t.bool,
   Number: t.number,
-  ID: t.variableName,
+  [IDS]: t.variableName,
   [strings]: t.string,
   [keywords]: t.keyword,
   [logicOp]: t.logicOperator,
